24b3ba16b54d638cc973009696e8d53657dbc4fc
NguyenPhamQuocHuy-20130283 <20130283@st.hcmuaf.edu.vn>
2023-12-05 22:08:33 +0700

    Update AddArticle.js

--- app/screens/AddArticle.js
+++ app/screens/AddArticle.js
@@ -1,158 +1,197 @@
 import React, { useState, useEffect } from "react";
 import {
+  Button,
+  Image,
   View,
-  Text,
   TextInput,
   TouchableOpacity,
+  Text,
   StyleSheet,
-  Image,
+  ScrollView,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import RNPickerSelect from "react-native-picker-select";
+import DropDownPicker from "react-native-dropdown-picker";
+import { Input } from "@rneui/themed/dist/Input";
 
-const AddArticle = () => {
-  const [title, setTitle] = useState("");
+export default function AddArticle() {
   const [image, setImage] = useState(null);
-  const [imageDescription, setImageDescription] = useState("");
-  const [content, setContent] = useState("");
-  const [category, setCategory] = useState("News");
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState(null);
+  const categories = [
+    { label: "Thể loại 1", value: "category1" },
+    { label: "Thể loại 2", value: "category2" },
+    // Thêm các thể loại khác nếu cần
+  ];
 
-  // Xử lý chọn ảnh từ thiết bị hoặc chụp ảnh mới
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
 
-    if (!result.cancelled) {
-      setImage(result.uri);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
     }
   };
 
-  // Xử lý gửi bài báo
-  const submitArticle = () => {
-    // Đưa logic xử lý gửi bài báo ở đây
+  const addArticle = () => {
+    // Perform logic to add article with title and image
     console.log("Title:", title);
-    console.log("Image:", image);
-    console.log("Image Description:", imageDescription);
-    console.log("Content:", content);
-    console.log("Category:", category);
+    console.log("Image URI:", image);
+    // Reset state after adding article
+    setTitle("");
+    setImage(null);
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Tiêu đề bài báo"
-        onChangeText={(text) => setTitle(text)}
-        value={title}
-      />
-
-      <TouchableOpacity style={styles.imagePickerButton} onPress={pickImage}>
-        <Text style={styles.buttonText}>Chọn ảnh</Text>
-      </TouchableOpacity>
-
-      {image && <Image source={{ uri: image }} style={styles.imagePreview} />}
-
-      <TextInput
-        style={styles.input}
-        placeholder="Mô tả ảnh"
-        onChangeText={(text) => setImageDescription(text)}
-        value={imageDescription}
-      />
-
-      <TextInput
-        style={styles.input}
-        placeholder="Nội dung bài báo"
-        multiline
-        onChangeText={(text) => setContent(text)}
-        value={content}
-      />
-
-      <RNPickerSelect
-        value={category}
-        onValueChange={(itemValue) => setCategory(itemValue)}
-        items={[
-          { label: "Thời sự", value: "News" },
-          { label: "Giải trí", value: "Entertainment" },
-          // Add other categories
-        ]}
-        style={pickerSelectStyles}
-      />
-
-      <TouchableOpacity style={styles.submitButton} onPress={submitArticle}>
-        <Text style={styles.buttonText}>Gửi bài báo</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.overlay}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Tiêu đề</Text>
+            <TextInput
+              style={styles.input}
+              value={title}
+              onChangeText={(text) => setTitle(text)}
+            />
+          </View>
+          <View style={styles.imagePickerContainer}>
+            <TouchableOpacity style={styles.button_image} onPress={pickImage}>
+              <Text style={styles.buttonText}>Chọn ảnh</Text>
+            </TouchableOpacity>
+            {image && (
+              <Image source={{ uri: image }} style={styles.selectedImage} />
+            )}
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Mô tả</Text>
+            <Input style={styles.input} />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Nội dung</Text>
+            <TextInput
+              style={styles.input_big}
+              multiline={true}
+              numberOfLines={30}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Thể loại</Text>
+            <DropDownPicker
+              items={categories}
+              placeholder="Chọn thể loại"
+              containerStyle={styles.dropdownContainer}
+              style={styles.dropdown}
+              itemStyle={styles.dropdownItem}
+              dropDownStyle={styles.dropdownDropdown}
+              onChangeItem={(item) => setCategory(item.value)}
+            />
+          </View>
+          <TouchableOpacity style={styles.button} onPress={addArticle}>
+            <Text style={styles.buttonText}>Thêm bài viết</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
-};
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 4,
-    color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: "purple",
-    borderRadius: 8,
-    color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
-});
+}
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1, // Đảm bảo ScrollView có thể mở rộng khi nội dung lớn hơn màn hình
+  },
   container: {
     flex: 1,
-    padding: 20,
+    fontFamily: "Inter-Bold",
+    justifyContent: "center",
+  },
+  overlay: {
+    backgroundColor: "white",
+    padding: 25,
+    marginHorizontal: 15,
+    borderRadius: 10,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  imagePickerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start", // Chỉnh căn lề về phía bên phải
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: "bold",
   },
   input: {
-    height: 40,
-    borderColor: "gray",
+    height: 45,
+    borderColor: "white",
+    backgroundColor: "#F0F0F0",
     borderWidth: 1,
-    marginBottom: 20,
-    padding: 10,
-  },
-  imagePickerButton: {
-    backgroundColor: "purple",
     padding: 10,
     borderRadius: 5,
-    alignItems: "center",
   },
-  imagePreview: {
-    width: 100,
-    height: 100,
-    resizeMode: "cover",
-    marginBottom: 20,
-  },
-  picker: {
-    height: 40,
-    borderColor: "gray",
+  input_big: {
+    height: 200,
+    borderColor: "white",
+    backgroundColor: "#F0F0F0",
     borderWidth: 1,
-    marginBottom: 20,
-  },
-  submitButton: {
-    backgroundColor: "purple",
-    padding: 15,
+    padding: 10,
     borderRadius: 5,
+    textAlignVertical: "top",
+  },
+  button: {
+    backgroundColor: "#6941DE",
+    borderRadius: 5,
+    padding: 10,
     alignItems: "center",
+    width: "50%",
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+  button_image: {
+    backgroundColor: "#6941DE",
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+    width: "30%",
+    alignSelf: "center",
+    marginBottom: 10,
+    marginRight: 10,
   },
   buttonText: {
     color: "white",
-    fontSize: 16,
+    fontSize: 17,
     fontWeight: "bold",
   },
+  selectedImage: {
+    width: 170,
+    height: 100,
+    borderRadius: 5,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  dropdownContainer: {
+    height: 40,
+    marginBottom: 20,
+  },
+  dropdown: {
+    backgroundColor: "#F0F0F0",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 5,
+  },
+  dropdownItem: {
+    justifyContent: "flex-start",
+  },
+  dropdownDropdown: {
+    backgroundColor: "#F0F0F0",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 5,
+  },
 });
-
-export default AddArticle;
