e3b086406df820cca4eb37abd238ee2a826fdbed
NguyenPhamQuocHuy-20130283 <20130283@st.hcmuaf.edu.vn>
2023-12-09 20:13:55 +0700

    just change some '' to "" and format it
    chỉ đổi do vscode nó tự định dạng "" với format thôi, tui chỉ vào xem chứ ko chỉnh gì đâu

--- App.js
+++ App.js
@@ -1,53 +1,53 @@
 // App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-
-import { Image } from 'react-native';
-import HienThi from './app/screens/user/HienThi';
-import TimKiem from './app/screens/user/TimKiem';
-import LichSu from './app/screens/user/LichSu';
-import Home from './app/screens/user/Home';
-import TaiKhoan from './app/screens/user/TaiKhoan';
-
-
+import { Image } from "react-native";
+import HienThi from "./app/screens/user/HienThi";
+import TimKiem from "./app/screens/user/TimKiem";
+import LichSu from "./app/screens/user/LichSu";
+import Home from "./app/screens/user/Home";
+import TaiKhoan from "./app/screens/user/TaiKhoan";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const TimKiemStack = () => (
   <Stack.Navigator>
-    <Stack.Screen name="TimKiem" component={TimKiem} options={{ headerShown: false }} />
-    <Stack.Screen name="HienThi" component={HienThi} options={{ headerShown: false }} />
+    <Stack.Screen
+      name="TimKiem"
+      component={TimKiem}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="HienThi"
+      component={HienThi}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
-
-
-
-
 const App = () => {
   return (
-    
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Home"
         tabBarOptions={{
-          activeTintColor: '#6941DE',
-          inactiveTintColor: 'gray',
+          activeTintColor: "#6941DE",
+          inactiveTintColor: "gray",
         }}
       >
         <Tab.Screen
           name="Home"
           component={Home}
           options={{
-            tabBarLabel: '',
+            tabBarLabel: "",
             tabBarIcon: ({ color, size }) => (
               <Image
-              source={require('./app/assets/home.png')} // Đường dẫn đến ảnh trong thư mục assets
-              style={{ width: size, height: size, tintColor: color }}
-            />
+                source={require("./app/assets/home.png")} // Đường dẫn đến ảnh trong thư mục assets
+                style={{ width: size, height: size, tintColor: color }}
+              />
             ),
           }}
         />
@@ -56,12 +56,12 @@ const App = () => {
           component={TimKiemStack}
           options={{
             headerShown: false,
-            tabBarLabel: '',
+            tabBarLabel: "",
             tabBarIcon: ({ color, size }) => (
               <Image
-              source={require('./app/assets/grid_view.png')} // Đường dẫn đến ảnh trong thư mục assets
-              style={{ width: size, height: size, tintColor: color }}
-            />
+                source={require("./app/assets/grid_view.png")} // Đường dẫn đến ảnh trong thư mục assets
+                style={{ width: size, height: size, tintColor: color }}
+              />
             ),
           }}
         />
@@ -70,12 +70,12 @@ const App = () => {
           component={LichSu}
           options={{
             headerShown: false,
-            tabBarLabel: '',
+            tabBarLabel: "",
             tabBarIcon: ({ color, size }) => (
               <Image
-              source={require('./app/assets/history.png')} // Đường dẫn đến ảnh trong thư mục assets
-              style={{ width: size, height: size, tintColor: color }}
-            />
+                source={require("./app/assets/history.png")} // Đường dẫn đến ảnh trong thư mục assets
+                style={{ width: size, height: size, tintColor: color }}
+              />
             ),
           }}
         />
@@ -83,12 +83,12 @@ const App = () => {
           name="TaiKhoan"
           component={TaiKhoan}
           options={{
-            tabBarLabel: '',
+            tabBarLabel: "",
             tabBarIcon: ({ color, size }) => (
               <Image
-              source={require('./app/assets/account_box.png')} // Đường dẫn đến ảnh trong thư mục assets
-              style={{ width: size, height: size, tintColor: color }}
-            />
+                source={require("./app/assets/account_box.png")} // Đường dẫn đến ảnh trong thư mục assets
+                style={{ width: size, height: size, tintColor: color }}
+              />
             ),
           }}
         />
