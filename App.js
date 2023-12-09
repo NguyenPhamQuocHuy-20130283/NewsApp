2607185813cca4925108a39eca4061d21be112bc
NguyenPhamQuocHuy-20130283 <20130283@st.hcmuaf.edu.vn>
2023-12-09 20:54:15 +0700

    Add navigation and logout in AdminHome screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 // Trong App.js
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Admin from "./app/screens/Admin";
 import AdminHome from "./app/screens/AdminHome";
 import AddArticle from "./app/screens/AddArticle";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { Alert } from "react-native";
 
 const Stack = createStackNavigator();
 
@@ -46,10 +47,53 @@ const App = () => {
           name="Admin"
           component={Admin}
           options={{
-            headerShown: false, // Ẩn header của Admin
+            headerShown: false,
           }}
         />
-        <Stack.Screen name="AdminHome" component={AdminHome} />
+        <Stack.Screen
+          name="AdminHome"
+          component={AdminHome}
+          options={{
+            title: "Trang quản trị viên",
+            headerShown: true,
+            headerLeft: ({ navigation }) => (
+              (navigation = useNavigation()),
+              (
+                <TouchableOpacity
+                  onPress={() => {
+                    Alert.alert(
+                      "Đăng xuất",
+                      "Bạn có chắc chắn muốn đăng xuất?",
+                      [
+                        {
+                          text: "Hủy",
+                          onPress: () => console.log("Cancel Pressed"),
+                          style: "cancel",
+                        },
+                        {
+                          text: "Đăng xuất",
+                          onPress: () => {
+                            // Use navigation object here
+                            navigation.navigate("Admin");
+                            console.log("OK");
+                          },
+                        },
+                      ],
+                      { cancelable: false }
+                    );
+                  }}
+                >
+                  <Icon
+                    name="sign-out"
+                    size={30}
+                    color="white"
+                    style={{ marginLeft: 10 }}
+                  />
+                </TouchableOpacity>
+              )
+            ),
+          }}
+        />
         <Stack.Screen name="AddArticle" component={AddArticle} />
       </Stack.Navigator>
     </NavigationContainer>
